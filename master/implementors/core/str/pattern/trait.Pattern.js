448da68576a10835720c9b6c78b1ce9b74abc69b
MaidSafe-QA <qa@maidsafe.net>
2016-05-28 23:28:09 +0000

    Updated documentation.

--- master/implementors/core/str/pattern/trait.Pattern.js
+++ master/implementors/core/str/pattern/trait.Pattern.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['libc'] = [];implementors['mio'] = [];
+implementors['libc'] = [];implementors['regex_syntax'] = [];implementors['mio'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
