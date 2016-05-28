125b5dc3554b5bb235dac91d6c635265e9d512bd
MaidSafe-QA <qa@maidsafe.net>
2016-05-28 17:18:32 +0000

    Updated documentation.

--- master/implementors/std/sys_common/thread_info/trait.NewThread.js
+++ master/implementors/std/sys_common/thread_info/trait.NewThread.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['mio'] = [];
+implementors['bitflags'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
