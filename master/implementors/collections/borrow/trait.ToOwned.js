1836c314ab1161c48d713b7c084dfd3119e47b1f
MaidSafe-QA <qa@maidsafe.net>
2016-06-03 15:46:19 +0000

    Updated documentation.

--- master/implementors/collections/borrow/trait.ToOwned.js
+++ master/implementors/collections/borrow/trait.ToOwned.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];implementors['serde'] = [];implementors['mio'] = [];
+implementors['serde'] = [];implementors['regex_syntax'] = [];implementors['mio'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/nonzero/trait.Zeroable.js
+++ master/implementors/core/nonzero/trait.Zeroable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['bitflags'] = [];implementors['libc'] = [];implementors['serde'] = [];
+implementors['bitflags'] = [];implementors['serde'] = [];implementors['libc'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
