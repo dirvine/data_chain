cf2f00db645d878305ed27233940ce5924842059
MaidSafe-QA <qa@maidsafe.net>
2016-06-11 00:59:47 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.Shr.js
+++ master/implementors/core/ops/trait.Shr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['serde'] = [];implementors['mio'] = [];
+implementors['serde'] = [];implementors['libc'] = [];implementors['mio'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
