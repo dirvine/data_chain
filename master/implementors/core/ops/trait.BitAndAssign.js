fc99a518e57f5c57d408246a59f6324bf335d883
MaidSafe-QA <qa@maidsafe.net>
2016-06-11 01:09:55 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.BitAndAssign.js
+++ master/implementors/core/ops/trait.BitAndAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['serde'] = [];implementors['libc'] = [];implementors['mio'] = [];
+implementors['libc'] = [];implementors['serde'] = [];implementors['mio'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
