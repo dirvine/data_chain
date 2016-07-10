7531469386502c84c200cf31aaa4ee6fd2b2b0fd
MaidSafe-QA <qa@maidsafe.net>
2016-07-10 23:30:47 +0000

    Updated documentation.

--- master/implementors/rayon/par_iter/reduce/trait.ReduceOp.js
+++ master/implementors/rayon/par_iter/reduce/trait.ReduceOp.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['rayon'] = [];
+implementors["rayon"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
