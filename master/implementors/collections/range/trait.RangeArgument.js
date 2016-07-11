3c2c49dd1b90dd240e7161f42b013cd98b2d1c07
MaidSafe-QA <qa@maidsafe.net>
2016-07-11 16:30:42 +0000

    Updated documentation.

--- master/implementors/collections/range/trait.RangeArgument.js
+++ master/implementors/collections/range/trait.RangeArgument.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitflags"] = [];implementors["regex_syntax"] = [];implementors["serde"] = [];
+implementors["bitflags"] = [];implementors["serde"] = [];implementors["regex_syntax"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
