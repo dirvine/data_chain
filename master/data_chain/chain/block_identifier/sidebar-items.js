16205abe74ddbb7ba82caea83cd1df9c1e3b5249
MaidSafe-QA <qa@maidsafe.net>
2016-07-13 23:57:07 +0000

    Updated documentation.

--- master/data_chain/chain/block_identifier/sidebar-items.js
+++ master/data_chain/chain/block_identifier/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["BlockIdentifier","Data identifiers for use in a data Chain. The hash of each data type is available to ensure there is no confusion  over the validity of any data presented by this chain "]]});
+initSidebarItems({"enum":[["BlockIdentifier","Data identifiers for use in a data Chain. The hash of each data type is available to ensure there is no confusion over the validity of any data presented by this chain"]]});
