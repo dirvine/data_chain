c511b19b95c8d4669b5454dbb674895a6dc9702d
MaidSafe-QA <qa@maidsafe.net>
2016-07-15 02:43:31 +0000

    Updated documentation.

--- master/implementors/collections/range/trait.RangeArgument.js
+++ master/implementors/collections/range/trait.RangeArgument.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitflags"] = [];implementors["regex_syntax"] = [];implementors["serde"] = [];
+implementors["serde"] = [];implementors["bitflags"] = [];implementors["regex_syntax"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/core/array/trait.FixedSizeArray.js
+++ master/implementors/core/array/trait.FixedSizeArray.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["bitflags"] = [];implementors["libc"] = [];implementors["serde"] = [];
+implementors["serde"] = [];implementors["bitflags"] = [];implementors["libc"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
