acaa8213476210d7e4469c44701c6e662232e853
MaidSafe-QA <qa@maidsafe.net>
2016-08-25 09:30:01 +0000

    Updated documentation.

--- master/implementors/collections/string/trait.ToString.js
+++ master/implementors/collections/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["regex_syntax"] = [];implementors["serde"] = [];implementors["mio"] = [];implementors["rust_sodium_sys"] = [];
+implementors["regex_syntax"] = [];implementors["mio"] = [];implementors["serde"] = [];implementors["rust_sodium_sys"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
