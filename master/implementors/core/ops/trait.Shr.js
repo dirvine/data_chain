973ee6148a367128c9137a354919ac65c7c8dda4
MaidSafe-QA <qa@maidsafe.net>
2017-01-20 11:40:37 +0000

    Updated documentation.

--- master/implementors/core/ops/trait.Shr.js
+++ master/implementors/core/ops/trait.Shr.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["either"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["nix"] = [];implementors["chrono"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];
+implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["either"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["chrono"] = [];implementors["nix"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- master/implementors/rand/reseeding/trait.Reseeder.js
+++ master/implementors/rand/reseeding/trait.Reseeder.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["lazy_static"] = [];implementors["antidote"] = [];implementors["humantime"] = [];implementors["libc"] = [];implementors["regex_syntax"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rand"] = [];implementors["nix"] = [];implementors["chrono"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["rust_sodium_sys"] = [];
+implementors["antidote"] = [];implementors["lazy_static"] = [];implementors["humantime"] = [];implementors["regex_syntax"] = [];implementors["libc"] = [];implementors["num"] = [];implementors["itertools"] = [];implementors["serde"] = [];implementors["serde"] = [];implementors["rand"] = [];implementors["chrono"] = [];implementors["nix"] = [];implementors["mio"] = [];implementors["ws"] = [];implementors["ws"] = [];implementors["maidsafe_utilities"] = [];implementors["maidsafe_utilities"] = [];implementors["rust_sodium_sys"] = [];implementors["rust_sodium_sys"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
